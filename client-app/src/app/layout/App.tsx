18db9ba2ccbaf54b3c7adcb10b0045197fb938dd
NhungTranPY <nhungtranpy91@gmail.com>
2021-06-05 15:29:48 +0700

    End of section 6

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,19 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import { Container } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import {v4 as uuid} from 'uuid';
+import agent from '../api/agent';
+import LoadingComponent from './LoadingComponent';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
   const [editMode, setEditMode] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [submitting, setSubmitting] = useState(false);
 
   useEffect(() => {
-    axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => {
-      setActivities(response.data)
+    agent.Activities.list().then(response => {
+      let activities: Activity[] = [];
+      response.forEach(activity => {
+        activity.date = activity.date.split('T')[0];
+        activities.push(activity);
+      })
+      setActivities(activities);
+      setLoading(false);
     })
   }, [])
 
@@ -35,16 +44,35 @@ function App() {
   }
 
   function handleCreateOrEditActivity(activity: Activity) { //bai 56
-    activity.id 
-      ? setActivities([...activities.filter(x => x.id !== activity.id), activity]) //update (bai 55): giu nguyen may activitis ma id ko trung voi cai id cua activity dang hien thi, sau do them cai updated activity nay vao activities
-      : setActivities([...activities, {...activity, id: uuid()}]) //tao moi them 1 activity va them vao activities (bai 55)
-    setEditMode(false);
-    setSelectedActivity(activity); //hien thi cai activity vua update/create
+    setSubmitting(true);
+    if (activity.id) { //bai 64: update
+      agent.Activities.update(activity).then(() => {
+        setActivities([...activities.filter(x => x.id !== activity.id), activity]) //update (bai 55 & 64): giu nguyen may activitis ma id ko trung voi cai id cua activity dang hien thi, sau do them cai updated activity nay vao activities
+        setSelectedActivity(activity); //hien thi cai activity vua update/create
+        setEditMode(false);
+        setSubmitting(false);
+      })
+    } else {
+      activity.id = uuid(); //bai 64: create
+      agent.Activities.create(activity).then(() => {
+        setActivities([...activities, activity]) //tao moi them 1 activity va them vao activities (bai 55 & 64)
+        setSelectedActivity(activity); //hien thi cai activity vua update/create
+        setEditMode(false);
+        setSubmitting(false);
+      })
+    }
   }
 
   function handleDeleteActivity(id: string) {
-    setActivities([...activities.filter(x => x.id !== id)]) //bai 58: xoa cai activity co id truyen vao thoi con giu lai cac activities con lai
+    setSubmitting(true);
+    agent.Activities.delete(id).then(() => {
+      setActivities([...activities.filter(x => x.id !== id)]) //bai 58: xoa cai activity co id truyen vao thoi con giu lai cac activities con lai
+      setSubmitting(false);
+    })
+    
   }
+
+  if (loading) return <LoadingComponent content='Loading app' />
  
   return (
     <>
@@ -60,6 +88,7 @@ function App() {
           closeForm={handleFormClose}
           createOrEdit={handleCreateOrEditActivity}
           deleteActivity={handleDeleteActivity}
+          submitting={submitting}
         />
       </Container>
     </>
